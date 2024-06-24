6c21905b99defab5b718f325836bb1e5922e1b04
AdamNavarroLiriano <a.navarro0608@gmail.com>
2024-06-24 23:07:38 +0200

    added extraction of player pages

--- src/extraction/extraction.py
+++ src/extraction/extraction.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def find_player_pid(player_name: str) -> str | None:
@@ -33,3 +34,33 @@ def find_player_pid(player_name: str) -> str | None:
     ][0]
 
     return player_profile
+
+
+def find_player_pages(pid: str) -> int:
+    """
+    Finds the player's pages of games on the Chessgames.com website.
+
+    Args:
+        pid (str): The player's ID (pid).
+
+    Returns:
+       int: The number of pages of games found for a given player
+    """
+
+    base_url = "https://www.chessgames.com/perl/chess.pl?page={page}&pid={pid}"
+
+    with requests.Session() as s:
+        r = s.get(
+            base_url.format(page=1, pid=pid),
+            headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+            },
+        )
+        html = BeautifulSoup(r.content, "html.parser")
+
+    table_games = html.find_all("table")[1]
+
+    page_numbers = pd.read_html(str(table_games))[0]
+    page_numbers = page_numbers[0].iloc[0].split(";")[0].split("of ")[1]
+
+    return page_numbers
