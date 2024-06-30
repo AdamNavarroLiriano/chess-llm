5449235a3c9050e9064c9d9efe4fd2b8eea256b9
AdamNavarroLiriano <a.navarro0608@gmail.com>
2024-06-30 22:41:55 +0200

    code for scrapping

--- src/main.py
+++ src/main.py
@@ -1,24 +1,39 @@
 from extraction.extraction import PlayerGames, PageGames, GameScrapper
+import time
+import random
+from multiprocessing import Pool
+import pandas as pd
+import os
 
 PLAYER = "Magnus Carlsen"
 MIN_YEAR = 2014
 MAX_YEAR = 2024
 
 
-def get_game_data(gid: str) -> tuple:
+def get_game_data(gid: str) -> dict[str, str | list[tuple[str]]]:
+    """Gets data from a game, using the game id (gid)
+
+    Args:
+        gid (str): game id from chessgames.com website
+
+    Returns:
+        dict[str, str | list[tuple[str]]]: dictionary with game data or
+        None if unable to get data
+    """
     game_scrapper = GameScrapper(gid)
 
     result = game_scrapper.result
+    game_type = game_scrapper.game_type
 
     try:
-        fens = game_scrapper.fens
+        pgn = game_scrapper.pgn
+        fens = game_scrapper.convert_to_fen()
     except:
         return None
 
-    pgn = game_scrapper.pgn
-    game_type = game_scrapper.game_type
+    time.sleep(random.uniform(3, 10))
 
-    return {"result": result, "pgn": pgn, "game_type": game_type, "fens": fens}
+    return (gid, {"result": result, "pgn": pgn, "game_type": game_type, "fens": fens})
 
 
 def main():
@@ -34,4 +49,19 @@ def main():
 
 if __name__ == "__main__":
     player_games = main()
-    player_games["data"] = player_games["gid"].apply(lambda x: get_game_data(x))
+
+    # Remove Blitz games, Simultaneous, Chess.com, 960 and lichess games
+    events_out = "(blitz)|(bullet)|(simultaneous)|(simul)|(960)|(lichess)|(exhibition)|(speed)|(chess.com)|(fischer)|(titled)"
+    player_games = player_games.loc[
+        ~player_games["event/locale"].str.lower().str.contains(events_out, regex=True)
+    ].reset_index(drop=True)
+
+    # Export
+    player_games = player_games.drop(columns="links")
+
+    # Get game data
+    player_games.to_parquet("../data/player_games.parquet")
+    player_games = pd.read_parquet("../data/player_games.parquet")
+
+    with Pool(8) as p:
+        game_data = p.map(get_game_data, player_games["gid"].tolist())
